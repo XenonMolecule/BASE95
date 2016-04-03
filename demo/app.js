27352cf079e6aa7f7f05049884c178e65ee4461b
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-04-03 11:31:49 +0000

    testing demo

--- demo/app.js
+++ demo/app.js
@@ -1,14 +1,19 @@
 // JavaScript File
 var base95 = new b95("481591570349-24nfhrrad8rbjmqf5154elakm10ot6n6.apps.googleusercontent.com","0B6IAOFaUVFIgSjRmc3cwYWpSZEk");
-var clicks;
-try {
-    base95.read("clicks");
-} catch(e){
-    console.log("ERROR: "+e);
+function everything(){
+    var clicks;
+    try {
+        base95.read("clicks");
+    } catch(e){
+        console.log("ERROR: "+e);
+    }
+    
+    $("button").click(function(){
+       clicks+=1;
+       base95.add("clicks",clicks);
+       console.log(clicks)
+       $("p").text("This button has been clicked "+clicks+"times");
+    });
 }
 
-$("button").on("click",function(){
-   clicks+=1;
-   base95.add("clicks",clicks);
-   $("p").text("This button has been clicked "+clicks+"times");
-});
+setTimeout(everything,8000);
