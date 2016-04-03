458af8d1e094e6812fee18a1cf8ee5837031237a
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-04-03 13:59:11 +0000

    maybe fix

--- demo/app.js
+++ demo/app.js
@@ -7,6 +7,7 @@ function everything(){
         $("#paragraph").text("This button has been clicked "+clicks+" times");
     } catch(e){
         console.log("ERROR: "+e);
+        base95.add("clicks",0);
     }
     
     $("#buttonToClick").click(function(){
