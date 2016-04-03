c7c980ddc4fbde7735f474960f303d97f5e0fa1f
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-04-03 11:38:50 +0000

    SRSLY, the / didn't add on earlier commit

--- demo/app.js
+++ demo/app.js
@@ -3,7 +3,7 @@ var base95 = new b95("481591570349-24nfhrrad8rbjmqf5154elakm10ot6n6.apps.googleu
 function everything(){
     var clicks;
     try {
-        clicks = base95.read("clicks");
+        clicks = base95.read("clicks/");
     } catch(e){
         console.log("ERROR: "+e);
     }
