075872b49bae1934bb2afc073031ecb7d0e29822
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-04-03 14:13:27 +0000

    fix maybe

--- demo/app.js
+++ demo/app.js
@@ -1,7 +1,8 @@
 // JavaScript File
 var base95 = new b95("30133579980-tn3o4580eqpumfj2rk03399i3j8ku8cb.apps.googleusercontent.com","0B6IAOFaUVFIgZ0dPNzQydkxkbUk");
+var clicks;
 function everything(){
-    var clicks;
+    clicks;
     try {
         clicks = base95.read("clicks/");
         $("#paragraph").text("This button has been clicked "+clicks+" times");
