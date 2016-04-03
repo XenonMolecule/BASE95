bd3dbadbedc7517cdd9b494aa858be37ecf493b4
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-04-03 14:31:17 +0000

    tries to synch text and hidden box

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,7 @@
 // JavaScript File
 var base95 = new b95("30133579980-tn3o4580eqpumfj2rk03399i3j8ku8cb.apps.googleusercontent.com","0B6IAOFaUVFIgZ0dPNzQydkxkbUk");
 var clicks;
+var pastVal = "";
 function everything(){
     clicks;
     try {
@@ -28,3 +29,16 @@ $(document).resize(function(){
 });
 
 $("#buttonToClick").css("margin-top",(window.innerHeight/2)-150+"px");
+
+$("#BASE95Data").change(function(){
+   clicks = base95.read("clicks/"); 
+});
+
+function update(){
+    var value = $("#BASE95Data").val();
+    if (value!=pastVal){
+        clicks = base95.read("clicks/");
+    }
+    pastVal = value;
+    setTimeout(update, 200);
+}
